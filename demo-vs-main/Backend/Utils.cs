e5f39d90132735391f37f1f35b9d3e66908ebe67
equ1p <vaniavania.tim@gmail.com>
2026-04-29 23:17:01 +0200

    Update 29.04

---
+++ demo-vs-main/Backend/Utils.cs
@@ -0,0 +1,44 @@
+﻿using Raven.Client.Documents;
+
+namespace TasksVs.Backend;
+
+public static class Utils
+{
+    public static void PrintHelp()
+    {
+        const string helpTable = """
+                                 Usage:
+                                 0 - Generate documents
+                                 1 - Open Studio
+                                 2 - Test auto-textual index
+                                 3 - Test auto-numerical index
+                                 4 - Test auto-index with RavenVector
+                                 5 - Test static index
+                                 6 - Test static index with quantization
+                                 7 - Test embeddings generation task
+                                 8 - Quit
+                                 """;
+
+        Console.WriteLine(helpTable);
+    }
+
+    public static void GenerateDocuments()
+    {
+        var store = DocumentStoreHolder.Store;
+        using var httpClient = new HttpClient();
+        httpClient.Send(new HttpRequestMessage(HttpMethod.Post,
+            $"{store.Urls[0]}/databases/{store.Database}/studio/sample-data"));
+
+        using var bulkInsert = store.BulkInsert();
+        for (int i = 0; i < 512; ++i)
+        {
+            bulkInsert.Store(new MyOwnProduct() { MyVector = [RandomFloat(), RandomFloat()] });
+            bulkInsert.Store(new MyOwnProductWithRavenVector()
+                { MyVector = new RavenVector<float>(new float[] { RandomFloat(), RandomFloat() }) });
+        }
+
+        return;
+
+        float RandomFloat() => Random.Shared.NextSingle() * (Random.Shared.Next() % 2 == 0 ? 1f : -1f);
+    }
+}
---
+++ demo-vs-main/Program.cs
@@ -0,0 +1,60 @@
+﻿using Raven.Embedded;
+using TasksVs;
+using TasksVs.Backend;
+
+_ = DocumentStoreHolder.Store;
+Utils.PrintHelp();
+
+while (Console.ReadLine() is { } s)
+{
+    if (int.TryParse(s, out var result) == false)
+    {
+        Utils.PrintHelp();
+        continue;
+    }
+
+    switch (result)
+    {
+        case 0:
+            Console.WriteLine("Generating documents...");
+            Utils.GenerateDocuments();
+            Console.WriteLine("Done");
+            break;
+
+        case 1:
+            EmbeddedServer.Instance.OpenStudioInBrowser();
+            break;
+        
+        case 2:
+            new AutoIndexesExercises().TestTextual();
+            break;
+
+        case 3:
+            new AutoIndexesExercises().TestNumerical();
+            break;
+
+        case 4:
+            new AutoIndexesExercises().TestRavenVector();
+            break;
+
+        case 5:
+            new StaticIndexesExercises().TextualSimpleIndex();
+            break;        
+        
+        case 6:
+            new StaticIndexesExercises().NumericalSimpleIndexWithInt8Quantization();
+            break;
+        
+        case 7:
+            new EmbeddingsGenerationTasksExercises().SimpleEmbeddingsGenerationTask();
+            break;
+            
+        case 8:
+            return;
+
+        default:
+            Console.WriteLine($"Unknown option {s}.");
+            Utils.PrintHelp();
+            break;
+    }
+}
